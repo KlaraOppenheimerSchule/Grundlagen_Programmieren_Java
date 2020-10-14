b463ebdcc75d8989c5560d44b6c628e99e2a2c40
ArchieTheRaccoon <71369501+ArchieTheRaccoon@users.noreply.github.com>
2020-10-14 12:42:24 +0200

    owo

--- 01_Einfuehrung_OOP/01_Einfuehrung/Rafail Melikian/src/com/exercises/Third.java
+++ 01_Einfuehrung_OOP/01_Einfuehrung/Rafail Melikian/src/com/exercises/Third.java
@@ -1,39 +1,53 @@
 package com.exercises;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Third {
-    String classNumber = "";
-    public void main(String[] Args) {
+    public static void main(String[] Args) {
+        String classNumber = "";
         Scanner scanner = new Scanner(System.in);
-        System.out.print("First number:");
-        int first = scanner.nextInt();
-        System.out.print("Second number:");
-        int second = scanner.nextByte();
-        System.out.print("Third number:");
-        int third = scanner.nextByte();
-        System.out.print("Fourth number:");
-        int fourth = scanner.nextByte();
+        System.out.print("IP Adress: ");
+        String ipAdress = scanner.nextLine();
+        String[] arrayOfIpAdress = ipAdress.split("\\.");
+        System.out.println(Arrays.toString(arrayOfIpAdress));
+        int firstNumbersOfIP = Integer.parseInt(arrayOfIpAdress[0]);
+        System.out.println(firstNumbersOfIP);
 
-        if (first != 0) {
-            if (second != 0) {
-                if (third == 0) {
-                    if (fourth == 0) {
-                        classNumber = "Klasse D";
-                    } else {
-                        classNumber = "Klasse E";
-                    }
-                } else {
-                    classNumber = "Klasse C";
-                }
-            } else {
-                classNumber = "Klasse B";
-            }
+        int[] binaryArray = new int[4];
+        if ((firstNumbersOfIP / 128) == firstNumbersOfIP % 128) {
+            firstNumbersOfIP = firstNumbersOfIP - 128;
+            binaryArray[0] = 1;
         } else {
-            classNumber = "Klasse A";
+            binaryArray[0] = 0;
         }
 
-        System.out.println(classNumber);
+        if ((firstNumbersOfIP / 64) == firstNumbersOfIP % 64) {
+            firstNumbersOfIP = firstNumbersOfIP - 64;
+            binaryArray[1] = 1;
+        } else {
+            binaryArray[1] = 0;
+        }
+
+        if ((firstNumbersOfIP / 32) == firstNumbersOfIP % 32) {
+            firstNumbersOfIP = firstNumbersOfIP - 32;
+            binaryArray[2] = 1;
+        } else {
+            binaryArray[2] = 0;
+        }
+
+        if ((firstNumbersOfIP / 16) == firstNumbersOfIP % 16) {
+            firstNumbersOfIP = firstNumbersOfIP - 16;
+            binaryArray[3] = 1;
+        } else {
+            binaryArray[3] = 0;
+        }
+
+        if (binaryArray[0] == 0) {
+            classNumber = "Klasse A";
+        } else {
+            classNumber = "Bitch";
+        }
     }
 
 }
