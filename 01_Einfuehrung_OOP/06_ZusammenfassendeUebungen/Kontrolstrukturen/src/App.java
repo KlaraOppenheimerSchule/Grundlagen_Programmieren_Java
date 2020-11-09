75d4153bd2a77564fa25034eb12602e688095b40
hramospacheco <hramospacheco@hotmail.com>
2020-11-09 23:04:00 +0100

    add a project

---
+++ 01_Einfuehrung_OOP/06_ZusammenfassendeUebungen/Kontrolstrukturen/src/App.java
@@ -0,0 +1,7 @@
+import com.klara.Core.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Core.getInstance().runKosten();
+    }
+}
---
+++ 01_Einfuehrung_OOP/06_ZusammenfassendeUebungen/Kontrolstrukturen/src/com/klara/Core/Core.java
@@ -0,0 +1,42 @@
+package com.klara.Core;
+import java.util.Scanner;
+
+import org.graalvm.compiler.nodes.extended.PluginFactory_NullCheckNode;
+
+
+public class Core {
+    private static final Core OBJ = new Core();
+    private float liter;
+    private String sorte;
+
+    private int punkte;
+
+    private final float maxLiter = 100.0f;
+    private Scanner s;
+      private Core() {
+          System.out.println("Objekt gebildet...");
+          s = new Scanner(System.in);
+      }
+          
+      public static Core getInstance() {
+        return OBJ;
+      }
+      public void runKosten(){
+        System.out.println("Bitte Schreiben Sie die getankte Sorte");
+        this.sorte = s.nextLine();
+        System.out.println("Bitte Schreiben Sie die Liter");
+        this.liter = Float.parseFloat(s.nextLine());
+        s.close();
+        if(this.liter>= maxLiter)
+            System.out.println("Prima!, Rabbat von"+this.sorte);
+      }
+
+      public void runIHKpunkte(){
+        System.out.println("Bitte Schreiben Sie die Punkte");
+        this.punkte = Integer.parseInt(s.nextLine());
+        if(this.punkte < 0 || this.punkte > 100)
+            System.out.println("Fehler");
+        //switch
+      }
+
+}
