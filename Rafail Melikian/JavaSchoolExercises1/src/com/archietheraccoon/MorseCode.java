454f27b424eb515b89e9e17290d80a64696c18b7
Archiebolt <derfuchsbear@gmail.com>
2021-01-20 09:06:19 +0100

    finished MorseCode Exercise

---
+++ Rafail Melikian/JavaSchoolExercises1/src/com/archietheraccoon/MorseCode.java
@@ -0,0 +1,63 @@
+package com.archietheraccoon;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class MorseCode {
+    public static void main(String[] Args) {
+        Map<String, String> morseAlphabet = new HashMap<>();
+
+        morseAlphabet.put("A", ". -");
+        morseAlphabet.put("B", "- . . .");
+        morseAlphabet.put("C", "- . - .");
+        morseAlphabet.put("D", "- . .");
+        morseAlphabet.put("E", ".");
+        morseAlphabet.put("F", ". . - .");
+        morseAlphabet.put("G", "- - .");
+        morseAlphabet.put("H", ". . . .");
+        morseAlphabet.put("I", ". .");
+        morseAlphabet.put("J", ". - - -");
+        morseAlphabet.put("K", "- . -");
+        morseAlphabet.put("L", ". - . .");
+        morseAlphabet.put("M", "- -");
+        morseAlphabet.put("N", "- .");
+        morseAlphabet.put("O", "- - -");
+        morseAlphabet.put("P", ". - - .");
+        morseAlphabet.put("Q", "- - . -");
+        morseAlphabet.put("R", ". - .");
+        morseAlphabet.put("S", ". . .");
+        morseAlphabet.put("T", "-");
+        morseAlphabet.put("U", ". . -");
+        morseAlphabet.put("V", ". . . -");
+        morseAlphabet.put("W", ". - -");
+        morseAlphabet.put("X", "- . . -");
+        morseAlphabet.put("Y", "- . - -");
+        morseAlphabet.put("Z", "- - . .");
+        morseAlphabet.put("1", ". - - - -");
+        morseAlphabet.put("2", ". . - - -");
+        morseAlphabet.put("3", ". . . - -");
+        morseAlphabet.put("4", ". . . . -");
+        morseAlphabet.put("5", ". . . . .");
+        morseAlphabet.put("6", "- . . . .");
+        morseAlphabet.put("7", "- - . . .");
+        morseAlphabet.put("8", "- - - . .");
+        morseAlphabet.put("9", "- - - - .");
+        morseAlphabet.put("0", "- - - - -");
+        morseAlphabet.put(" ", "  ");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("InputToMorse: ");
+        String userInput = scanner.nextLine().toUpperCase();
+
+        StringBuilder sentence = new StringBuilder();
+
+        for (int i = 0; i < userInput.length(); i++) {
+            sentence.append(" ");
+            sentence.append(morseAlphabet.get(String.valueOf(userInput.charAt(i))));
+            sentence.append(" ");
+        }
+
+        System.out.println(sentence);
+    }
+}
